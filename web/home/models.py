865d16864dc6bf4a347712ecc692bf14cb9eaa77
RJ Nay <rjnay1984@gmail.com>
2020-09-16 14:48:59 -0500

    update environment and add test

--- web/home/models.py
+++ web/home/models.py
@@ -1,9 +1,8 @@
-from django.db import models
-
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.api import APIField
+
 
 class HomePage(Page):
     body = RichTextField(blank=True)
