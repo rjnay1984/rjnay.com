1ca5d207d843115622925dce1b5707c18b976395
RJ Nay <rjnay1984@gmail.com>
2020-09-21 15:33:57 -0500

    basic home component and slice

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import homeReducer from '../features/home/homeSlice';
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
+    homeContent: homeReducer,
   },
 });
---
+++ frontend/src/features/home/Home.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+
+// import the useDispatch Redux hook
+import { useDispatch, useSelector } from 'react-redux';		
+
+// import our fetchRecipes thunk
+import { fetchHomeContent, homeSelector } from './homeSlice';
+
+function createMarkup(body) {
+    return {__html: body }
+}
+
+export function Home() {
+    const dispatch = useDispatch();
+    const { loading, content, hasErrors } = useSelector(homeSelector);
+
+    useEffect(() => {
+        dispatch(fetchHomeContent())
+    }, [dispatch])
+
+    if (loading) return <div>Loading...</div>
+    if (hasErrors) return <div>Error loading content.</div>
+
+    return (
+        <div>
+            <div dangerouslySetInnerHTML={createMarkup(content.body)} />
+        </div>
+    );
+}
+
